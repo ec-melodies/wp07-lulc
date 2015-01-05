f2d6427c2d7f42e1ce54e57700ebaa8f873564c4
vascobnunes <vascobnunes@gmail.com>
2015-01-05 14:49:45 +0000

    setup environment variables

--- main.sh
+++ main.sh
@@ -2,8 +2,14 @@
 
 # User specific environment and startup programs
 
-PATH=$PATH:$HOME/bin
 
+Landsat_LDOPE=/application/Landsat_LDOPE/linux64bit_bin
+applibs=./libs/
+PATH=$PATH:$HOME/bin:$applibs:/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
+
+export LD_LIBRARY_PATH=/usr/local/lib/otb/
+export PYTHONPATH=/usr/local/grass-6.4.3/etc/python:$applibs
+export GDAL_DATA=/application/gdal
 export PATH
 
-python /bin/lulc_main.py
+python ./bin/lulc_main.py
