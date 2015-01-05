eb13f5eafac6060bfcb532a7f365119275aaa6d7
vascobnunes <vascobnunes@gmail.com>
2015-01-05 17:42:15 +0000

    initialization files
    Setting up environment variables
    Directing to lulc_main.py

---
+++ ini.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# verify 
+cat $HOME/wp07-lulc/main.sh
+ 
+# make it executable
+chmod u+x $HOME/wp07-lulc/main.sh
+chmod u+x $HOME/wp07-lulc/lib/r.in.landsat.new.py
+chmod u+x $HOME/wp07-lulc/lib/i.lulc.national.py
+ 
+# define job file as environmental variable
+export GRASS_BATCH_JOB=$HOME/wp07-lulc/main.sh
+export PATH=$PATH:$HOME/wp07-lulc/lib
+export PYTHONPATH=$PYTHONPATH:$HOME/wp07-lulc/lib:$HOME/LANDSAT-Download
+export LD_LIBRARY_PATH=/usr/local/lib/otb/
+export GDAL_DATA=/application/gdal
+ 
+# run the job
+grass64 -text ~/wp07-lulc/GRASS_data/World/National/
+# or
+# grass70 ~/grassdata/nc_spm_08_grass7/user1
+ 
+# switch back to interactive mode
+unset GRASS_BATCH_JOB
--- main.sh
+++ main.sh
@@ -1,15 +1,6 @@
-#!/bin/bash
+chmod u+x /home/melodies-wp7/wp07-lulc/bin/lulc_main.py
+python /home/melodies-wp7/wp07-lulc/bin/lulc_main.py
 
-# User specific environment and startup programs
 
 
-Landsat_LDOPE=/application/Landsat_LDOPE/linux64bit_bin
-applibs=./libs/
-PATH=$PATH:$HOME/bin:$applibs:/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 
-export LD_LIBRARY_PATH=/usr/local/lib/otb/
-export PYTHONPATH=/usr/local/grass-6.4.3/etc/python:$applibs
-export GDAL_DATA=/application/gdal
-export PATH
-
-python ./bin/lulc_main.py
