c1b2cd754fa3f4e6c46d0d25f623c7cfc8c0790c
vascobnunes <vascobnunes@gmail.com>
2015-01-23 18:34:21 +0000

    Implementing issue #1

--- lib/getlandsat.py
+++ lib/getlandsat.py
@@ -17,6 +17,36 @@
 import os, imp
 import subprocess
 
+def Calculate_season(image_aquisition,region):
+    if region=='Portugal':
+        if 173<int(image_aquisition[13:16])<295:
+            season='Dry'
+        else:
+            season='Wet'
+    # print season		
+    return season
+
+def checkseasonimg(year,season,tile,dir):
+    all=os.listdir(dir)
+    dirs=[]
+    imagefound=''
+    for f in all:
+        if os.path.isdir(os.path.join(dir,f))==True:
+            dirs.append(f)
+    print dirs			
+    for img in dirs:
+        tile_img=img[3:9]		
+        year_img=img[9:13]
+        season_img=Calculate_season(img,'Portugal')
+        if year_img==year and season_img==season and tile_img==tile:
+            imagefound=img
+            break
+    if imagefound!='':
+        return imagefound	
+    else:
+        return None
+
+
 def getlandsats(years,tiles,admitedcloudcover,outputdir):
     if admitedcloudcover==None:
         admitedcloudcover=''
@@ -31,12 +61,22 @@ def getlandsats(years,tiles,admitedcloudcover,outputdir):
     for tile in tiles:
         for year, bird in years.iteritems():
             for season in seasonintervals:
-                startdate=year+season.split('-')[0]
-                enddate=year+season.split('-')[1]
-                print "Search interval: " + startdate + " to " +enddate + " - Bird: " + bird
-                subprocess.call('download_landsat_scene.py -z unzip -b '+bird+' -o scene -d '+startdate+' -f '+enddate+admitedcloudcover+' -s '+tile+' -u '+passwfile+' --output '+outputdir, shell=True)
-                l = open(logfile,'r')
-                images=l.read().translate(None, '\n[]\'')
-                if images!='':		
-                    downloaded=downloaded+images.split(',')
+                if season=='0101-0630':
+                    season_ext='Wet'
+                else:
+                    season_ext=='Dry'
+                checkexists=checkseasonimg(year,season_ext,tile,outputdir)
+                print checkexists	
+                if checkexists!=None:
+                    print "Bingo! There is already an image in the output directory with year, tile and season corresponding to what you're searching: "+checkexists+". Skiping.."
+                    downloaded.append(checkexists)
+                else:
+                    startdate=year+season.split('-')[0]
+                    enddate=year+season.split('-')[1]
+                    print "Search interval: " + startdate + " to " +enddate + " - Bird: " + bird
+                    subprocess.call('download_landsat_scene.py -z unzip -b '+bird+' -o scene -d '+startdate+' -f '+enddate+admitedcloudcover+' -s '+tile+' -u '+passwfile+' --output '+outputdir, shell=True)
+                    l = open(logfile,'r')
+                    images=l.read().translate(None, '\n[]\'')
+                    if images!='':		
+                        downloaded=downloaded+images.split(',')
     return downloaded		
