711e87f598a3c2ea40e7f0d41f3b7d3ddbe2c55e
Vasco Nunes <vbnunes@criticalsoftware.com>
2015-05-06 11:21:28 +0100

    Implementing cloud filling - issue #36

--- src/main/app-resources/lib/icloudfill.py
+++ src/main/app-resources/lib/icloudfill.py
@@ -23,21 +23,22 @@ def cloudfill(output_suf, year, tile):
         else:
             output = year +"_band" + x  +"_" + output_suf +tile+ "_Dry"
             output_dry.append(output)	
-
+			
+    #run the loops for dry and wet seasons
     for idx, band in enumerate(output_wet):	
         grass.message("Filling clouds in " + band + " with data from " + output_dry[idx])
         try:
             grass.mapcalc("$tempV= if(isnull($Bwet),$Bdry,$Bwet)",tempV='tempV',Bwet=band,Bdry=output_dry[idx])
-            grass.mapcalc("$Bwet= $tempV",tempV='tempV',Bwet=band,Bdry=output_dry[idx])
+            grass.mapcalc("$Bwet= $tempV",tempV='tempV',Bwet=band)
             grass.run_command("g.remove", flags= 'f', rast = 'tempV', quiet=True)			
         except:
             grass.message('ERROR')
 
     for idx, band in enumerate(output_dry):	
-        grass.message("Filling clouds in " + band + " with data from " + output_dry[idx])
+        grass.message("Filling clouds in " + band + " with data from " + output_wet[idx])
         try:
             grass.mapcalc("$tempV= if(isnull($Bwet),$Bdry,$Bwet)",tempV='tempV',Bwet=band,Bdry=output_wet[idx])
-            grass.mapcalc("$Bwet= $tempV",tempV='tempV',Bwet=band,Bdry=output_dry[idx])
+            grass.mapcalc("$Bwet= $tempV",tempV='tempV',Bwet=band)
             grass.run_command("g.remove", flags= 'f', rast = 'tempV', quiet=True)			
         except:
             grass.message('ERROR')			
